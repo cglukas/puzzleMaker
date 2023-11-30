9db887a508fd64c09f2b656af327d57f5ee9017e
Lukas Wieg <lukaswieg@googlemail.com>
2023-11-30 13:53:22 +0100

    Implement drawing for the puzzle piece.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,5 +15,16 @@ MainWindow::~MainWindow() {}
  * */
 void MainWindow::buildUI(){
     PuzzleView *my_view = new PuzzleView();
+    PuzzlePiece p;
+    p.addVertex(10,10);
+    p.addVertex(10,100);
+    p.addVertex(100,100);
+    p.addVertex(100,10);
+    PuzzlePiece r;
+    r.addVertex(110,10);
+    r.addVertex(110,100);
+    r.addVertex(200,100);
+    r.addVertex(200,10);
+    my_view->drawPuzzlePieces({p,r});
     setCentralWidget(my_view);
 }
--- puzzleview.cpp
+++ puzzleview.cpp
@@ -7,10 +7,26 @@ PuzzleView::PuzzleView(QWidget *parent)
     : QWidget{parent}
 {}
 
+/*
+ * Set the puzzle pieces that should be drawn
+ * */
+void PuzzleView::drawPuzzlePieces(std::vector<PuzzlePiece> puzzle_pieces)
+{
+    this->puzzle_pieces = puzzle_pieces;
+}
+
 
 void PuzzleView::paintEvent(QPaintEvent *event){
     QPainter painter(this);
     painter.setPen(Qt::blue);
-    painter.setFont(QFont("Arial", 30));
-    painter.drawText(rect(), Qt::AlignCenter, "Qt");
+    for(PuzzlePiece piece : puzzle_pieces){
+        for(int i = 0; i < piece.getVertecies().size(); i++){
+            Vertex v1 = piece.getVertecies()[i];
+            int k = (i +1) % piece.getVertecies().size();
+            Vertex v2 = piece.getVertecies()[k];
+
+            painter.drawLine(v1.getX(), v1.getY(), v2.getX(), v2.getY());
+        }
+
+    }
 }
