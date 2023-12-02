9342a2721b8b4cd8445e4c781a735ec0500702ec
Lukas Wieg <lukaswieg@googlemail.com>
2023-12-02 17:47:44 +0100

    Add edge implementation to the puzzle pieces for accessing edges.

--- puzzle_piece.h
+++ puzzle_piece.h
@@ -1,27 +1,30 @@
 #ifndef PUZZLE_PIECE_H
 #define PUZZLE_PIECE_H
 
+#include "edge.h"
 #include "vertex.h"
 #include <vector>
 #include <QPoint>
 
 class PuzzlePiece
 {
+private:
+    int id;
+    static int current_id;
+    std::vector<Vertex> vertecies;
+
 public:
     PuzzlePiece(): id(current_id++) {};
     void addVertex(int x, int y);
     void addVertex(Vertex v);
     std::vector<Vertex> getVertecies();
+    std::vector<Edge> getEdges();
     bool operator==(PuzzlePiece const & other) const {
         return this->id == other.id;
     }
     bool operator!=(PuzzlePiece const & other) const {
         return this->id != other.id;
     }
-private:
-    int id;
-    static int current_id;
-    std::vector<Vertex> vertecies;
 };
 
 #endif // PUZZLE_PIECE_H
